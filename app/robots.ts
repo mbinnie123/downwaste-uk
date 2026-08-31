aa2b3ec853530f73d8ac56aa54c924fcdf24a2cf
Marcus Binnie <marcus_promo_d@yahoo.com>
2026-08-31 22:03:47 +0100

    Allow /estimator in robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/quote", "/estimator", "/api/"],
+        disallow: ["/quote", "/api/"],
       },
     ],
     sitemap: "https://downwaste.co.uk/sitemap.xml",
