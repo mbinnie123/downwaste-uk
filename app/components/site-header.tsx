23ae892f8d6728a01daf24158328e45c3b4fb278
Marcus Binnie <marcus_promo_d@yahoo.com>
2026-09-02 21:34:08 +0100

    Update header email to info@downwaste.co.uk

--- app/components/site-header.tsx
+++ app/components/site-header.tsx
@@ -127,7 +127,7 @@ export function SiteHeader() {
         <div className="flex items-center gap-2 sm:gap-3">
           <div className="hidden text-xs font-medium text-slate-700 sm:flex flex-col items-end">
             <a href="tel:+902362142590" className="hover:text-sky-700">+90 236 214 2590</a>
-            <a href="mailto:info@downwaste.com" className="hover:text-sky-700">info@downwaste.com</a>
+            <a href="mailto:info@downwaste.co.uk" className="hover:text-sky-700">info@downwaste.co.uk</a>
           </div>
 
           <Link
@@ -255,9 +255,9 @@ export function SiteHeader() {
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-4 w-4 text-slate-400"><path fillRule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clipRule="evenodd" /></svg>
               +90 236 214 2590
             </a>
-            <a href="mailto:info@downwaste.com" className="flex items-center gap-2 text-sm text-slate-600 hover:text-sky-700 transition-colors">
+            <a href="mailto:info@downwaste.co.uk" className="flex items-center gap-2 text-sm text-slate-600 hover:text-sky-700 transition-colors">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-4 w-4 text-slate-400"><path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z" /><path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z" /></svg>
-              info@downwaste.com
+              info@downwaste.co.uk
             </a>
           </div>
         </div>
